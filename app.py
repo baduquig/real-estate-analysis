7086fa80bb6eefbb737b7454011fae3832b7d503
Gabe Baduqui <baduquig@gmail.com>
2022-06-12 12:39:43 -0600

    learned how to add dropdown list in Dash

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ import pandas as pd
 app = Dash(__name__)
 
 df = pd.read_csv('./data/zvhi_3bed.csv')
+
+state_dropdown_values = df['State'].unique()
+
 df = df[df['State']=='AZ']
 df = df[df['City'].isin(['Chandler', 'Tempe', 'Mesa', 'Gilbert'])]
 df = df.drop(['RegionID', 'SizeRank', 'RegionName', 'RegionName', 'RegionType', 'StateName', 'State', 'Metro', 'CountyName'], axis=1)
@@ -15,10 +18,19 @@ df = df.transpose()
 fig = px.line(df)
 
 
-app.layout = html.Div(children=[
-    html.H1(children='3 bedroom housing prices in Phoenix, AZ'),
+app.layout = html.Div([
+    html.H1('3 bedroom housing prices in Phoenix, AZ'),
 
-    html.Div(children='Time series data analysis of 3 bedroom homes in the east valley'),
+    html.Div('Time series data analysis of 3 bedroom homes in the east valley'),
+
+    html.Div([
+        dcc.Dropdown(
+            state_dropdown_values,
+            'Select a state',
+            id='state',
+            multi=True
+        )
+    ]),
 
     dcc.Graph(
         id='example-graph',
