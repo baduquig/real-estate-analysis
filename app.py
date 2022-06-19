64ee5cc0f0df1fda348d8d450246e414535e4fdd
Gabe Baduqui <baduquig@gmail.com>
2022-06-18 18:39:55 -0600

    chained callbacks wip

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from dash import Dash, html, dcc
+from dash import Dash, html, dcc, Input, Output
 import plotly.express as px
 import pandas as pd
 
@@ -6,19 +6,25 @@ app = Dash(__name__)
 
 df = pd.read_csv('./data/zvhi_3bed.csv')
 
+lowest_level = 'State'
 state_dropdown_values = df['State'].unique()
-city_dropdown_values = df['City'].unique()
-zipcode_dropdown_values = df['RegionName'].unique()
 
-df = df[df['State']=='AZ']
-df = df[df['City'].isin(['Chandler', 'Tempe', 'Mesa', 'Gilbert'])]
-df = df.drop(['RegionID', 'SizeRank', 'RegionName', 'RegionName', 'RegionType', 'StateName', 'State', 'Metro', 'CountyName'], axis=1)
-df = df.groupby(['City']).mean()
-df = df.transpose()
+state = []
+city = []
+zipcode = []
 
+def set_plot(data_frame, lowest_level, state, city):
+    if lowest_level == 'State':
+        data_frame = data_frame[data_frame['State'].isin(state)]
+    else:
+        data_frame = data_frame[data_frame['State'].isin(state)]
+        data_frame = data_frame[data_frame['City'].isin(city)]        
+    data_frame = data_frame.groupby([lowest_level]).mean()
+    data_frame = data_frame.drop(['RegionID', 'SizeRank', 'RegionName', 'RegionName', 'RegionType', 'StateName', 'State', 'Metro', 'CountyName'], axis=1)
+    data_frame = data_frame.transpose()
+    return data_frame
 
-fig = px.line(df)
-
+fig = px.line(set_plot(df, lowest_level, state, city))
 
 app.layout = html.Div([
     html.H1('3 bedroom housing prices in Phoenix, AZ'),
@@ -26,21 +32,22 @@ app.layout = html.Div([
     html.Div('Time series data analysis of 3 bedroom homes in the east valley'),
 
     html.Div([
+        html.Label('State'),
         dcc.Dropdown(
             state_dropdown_values,
-            'State',
+            'AZ',
             id='state',
             multi=True
         ),
+
+        html.Label('City'),
         dcc.Dropdown(
-            city_dropdown_values,
-            'City',
             id='city',
             multi=True
         ),
+
+        html.Label('Zipcode(s)'),
         dcc.Dropdown(
-            zipcode_dropdown_values,
-            'Zipcode',
             id='zipcode',
             multi=True
         )
@@ -51,6 +58,26 @@ app.layout = html.Div([
         figure=fig
     )
 ])
+#~~~ End app.layout ~~~#
+
+#~~~ Callbacks ~~~#
+@app.callback(
+    Output('city', 'options'),
+    Input('state', 'value')
+)
+def set_cities(selected_states):
+    lowest_level = 'State'
+    return df[df['City'].isin([selected_states])].unique()
+
+@app.callback(
+    Output('zipcode', 'options'),
+    Input('city', 'value')
+)
+def zip_codes(selected_cities):
+    lowest_level = 'City'
+    return df[df['RegionName'].isin([selected_cities])].unique()
+#~~~ Callbacks ~~~#
+    
 
 if __name__ == '__main__':
     app.run_server(debug=True)
