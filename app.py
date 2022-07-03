24ff78621e0f58cc43dd249de933fd5646de91df
Gabe Baduqui <baduquig@gmail.com>
2022-07-03 17:43:46 -0600

    fixed issues with callbacks, when selecting new state

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+from re import A
+from select import select
 from dash import Dash, html, dcc, Input, Output
 import plotly.express as px
 import pandas as pd
@@ -46,21 +48,30 @@ app.layout = html.Div([
 #~~~ Callbacks ~~~#
 @app.callback(
     Output('city', 'options'),
-    Input('state', 'value')
+    Input('state', 'value'),
+    prevent_initial_call=True
 )
 def set_city_options(selected_states):
     cities = df[df['State'].isin(list(selected_states))]
-    return cities['City'].unique()
+    cities = cities['City'].unique()
+    return cities
+
 
 @app.callback(
     Output('zipcode', 'options'),
     Input('state', 'value'),
-    Input('city', 'value')
+    Input('city', 'value'),
+    prevent_initial_call=True
 )
 def set_zip_options(selected_states, selected_cities):
-    zip_codes = df[df['State'].isin(list(selected_states))]
-    zip_codes = zip_codes[zip_codes['City'].isin(list(selected_cities))]
-    return zip_codes['RegionName'].unique()
+    if (selected_cities is None or selected_cities == []):
+        zip_codes = []
+    else:
+        zip_codes = df[df['State'].isin(list(selected_states))]
+        zip_codes = zip_codes[zip_codes['City'].isin(list(selected_cities))]
+        zip_codes = zip_codes['RegionName'].unique()
+    return zip_codes
+
 
 @app.callback(
     Output('hpi-line-graph', 'figure'),
@@ -70,39 +81,31 @@ def set_zip_options(selected_states, selected_cities):
 )
 def set_city_plot(selected_states, selected_cities, selected_zipcodes):
     data_frame = df
-
-    if ((selected_states is None) and (selected_cities is None) and (selected_zipcodes is None)):
-        pass
-    elif((selected_cities is None) and (selected_zipcodes is None)):
+    print('selected_states: ' + str(selected_states))
+    print('selected_cities: ' + str(selected_cities))
+    print('selected_zipcodes: ' + str(selected_zipcodes))
+    if ((selected_states is None or selected_states == []) 
+    and (selected_cities is None or selected_cities == []) 
+    and (selected_zipcodes is None or selected_zipcodes == [])):
+        data_frame = data_frame.drop(['RegionID', 'SizeRank', 'RegionName', 'RegionType', 'StateName', 'State', 'City', 'Metro', 'CountyName'], axis=1)
+    elif((selected_cities is None or selected_cities == []) and (selected_zipcodes is None or selected_zipcodes == [])):
         data_frame = data_frame[data_frame['State'].isin(selected_states)]
-        data_frame = data_frame.drop(['RegionID', 'SizeRank', 'RegionName', 'RegionType', 'State', 'City', 'Metro', 'CountyName'], axis=1)
-    elif(selected_zipcodes is None):
+        data_frame = data_frame.drop(['RegionID', 'SizeRank', 'RegionName', 'RegionType', 'StateName', 'City', 'Metro', 'CountyName'], axis=1)
+        data_frame = data_frame.groupby('State')
+    elif(selected_zipcodes is None or selected_zipcodes == []):
         data_frame = data_frame[data_frame['State'].isin(selected_states)]
         data_frame = data_frame[data_frame['City'].isin(selected_cities)]
         data_frame = data_frame.drop(['RegionID', 'SizeRank', 'RegionName', 'RegionType', 'StateName', 'State', 'Metro', 'CountyName'], axis=1)
+        data_frame = data_frame.groupby('City')
     else:
         data_frame = data_frame[data_frame['State'].isin(selected_states)]
         data_frame = data_frame[data_frame['City'].isin(selected_cities)]
         data_frame = data_frame[data_frame['RegionName'].isin(selected_zipcodes)]
-        data_frame = data_frame.groupby('RegionName')
-        
+        data_frame = data_frame.groupby('RegionName')        
 
     data_frame = data_frame.mean()
     data_frame = data_frame.transpose()
     return px.line(data_frame)
-
-    """
-    if len(selected_cities) > 0:
-        data_frame = data_frame[data_frame['State'].isin(selected_states)]
-        data_frame = data_frame[data_frame['City'].isin(selected_cities)]
-    else: 
-        data_frame = data_frame[data_frame['State'].isin(selected_states)]
-
-    data_frame = data_frame.drop(['RegionID', 'SizeRank', 'RegionType', 'StateName', 'State', 'City', 'Metro', 'CountyName'], axis=1)
-    data_frame = data_frame.mean()
-    data_frame = data_frame.transpose()
-    return px.line(data_frame)
-    """
 
 #~~~ Callbacks ~~~#
     
