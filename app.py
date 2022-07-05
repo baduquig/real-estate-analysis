20d68dfd94a9a7eb595bf3c978ebe7578861d8d9
Gabe Baduqui <baduquig@gmail.com>
2022-07-05 14:01:14 -0600

    set plot legend label for 'US' and apply relevant axis label names

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-from re import A
-from select import select
 from dash import Dash, html, dcc, Input, Output
 import plotly.express as px
 import pandas as pd
@@ -8,14 +6,13 @@ from tenacity import retry
 app = Dash(__name__)
 
 df = pd.read_csv('./data/zvhi_3bed.csv')
+df['Country'] = 'US'
 
 state_dropdown_values = df['State'].unique()
 
 
 app.layout = html.Div([
     html.H1('3 bedroom housing prices in the United States'),
-
-    html.Div('Time series data analysis of 3 bedroom homes'),
 
     html.Div([
         html.Label('State'),
@@ -81,13 +78,12 @@ def set_zip_options(selected_states, selected_cities):
 )
 def set_city_plot(selected_states, selected_cities, selected_zipcodes):
     data_frame = df
-    print('selected_states: ' + str(selected_states))
-    print('selected_cities: ' + str(selected_cities))
-    print('selected_zipcodes: ' + str(selected_zipcodes))
+    
     if ((selected_states is None or selected_states == []) 
     and (selected_cities is None or selected_cities == []) 
     and (selected_zipcodes is None or selected_zipcodes == [])):
         data_frame = data_frame.drop(['RegionID', 'SizeRank', 'RegionName', 'RegionType', 'StateName', 'State', 'City', 'Metro', 'CountyName'], axis=1)
+        data_frame = data_frame.groupby('Country')
     elif((selected_cities is None or selected_cities == []) and (selected_zipcodes is None or selected_zipcodes == [])):
         data_frame = data_frame[data_frame['State'].isin(selected_states)]
         data_frame = data_frame.drop(['RegionID', 'SizeRank', 'RegionName', 'RegionType', 'StateName', 'City', 'Metro', 'CountyName'], axis=1)
@@ -101,11 +97,19 @@ def set_city_plot(selected_states, selected_cities, selected_zipcodes):
         data_frame = data_frame[data_frame['State'].isin(selected_states)]
         data_frame = data_frame[data_frame['City'].isin(selected_cities)]
         data_frame = data_frame[data_frame['RegionName'].isin(selected_zipcodes)]
-        data_frame = data_frame.groupby('RegionName')        
-
+        data_frame = data_frame.groupby('RegionName')
+    
     data_frame = data_frame.mean()
     data_frame = data_frame.transpose()
-    return px.line(data_frame)
+
+    print(data_frame)
+
+    fig = px.line(data_frame, labels={
+        'index': 'Year',
+        'value': 'Average Price'
+    })
+    
+    return fig
 
 #~~~ Callbacks ~~~#
     
